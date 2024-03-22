a905f18def1d7837ad6593903ccb918c4ff984bb
soulxj <xjplay@yahoo.com>
2024-03-22 09:59:16 +0800

    _1001The_Kerub_Threat 异常修复

--- GameServer/data/scripts/system/handlers/quest/poeta/_1001The_Kerub_Threat.java
+++ GameServer/data/scripts/system/handlers/quest/poeta/_1001The_Kerub_Threat.java
@@ -64,8 +64,9 @@ public class _1001The_Kerub_Threat extends QuestHandler
         final Player player = env.getPlayer();
 		final QuestState qs = player.getQuestStateList().getQuestState(questId);
 		int targetId = env.getTargetId();
-        int var = qs.getQuestVarById(0);
+
         if (qs != null && qs.getStatus() == QuestStatus.START) {
+			int var = qs.getQuestVarById(0);
             if (targetId == 203071) {
                 switch (env.getDialog()) {
                     case START_DIALOG: {
