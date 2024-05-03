9ab1fd5905477f9afba8be204cdc197266dbf9d3
soulxj <xjplay@yahoo.com>
2024-05-04 00:06:41 +0800

    fix cs ls build mistake

--- ChatServer/dist/StartCS.bat
+++ ChatServer/dist/StartCS.bat
@@ -6,7 +6,7 @@ IF "%MODE%" == "" (
 CALL PanelCS.bat
 )
 ECHO Starting AION Chat Server in %MODE% mode.
-JAVA %JAVA_OPTS% -cp ./lib/*;chatserver.jar com.aionemu.chatserver.ChatServer
+JAVA %JAVA_OPTS% -cp ./libs/*;AL-Chat.jar com.aionemu.chatserver.ChatServer
 SET CLASSPATH=%OLDCLASSPATH%
 IF ERRORLEVEL 2 GOTO START
 IF ERRORLEVEL 1 GOTO ERROR
--- LoginServer/dist/StartLS.bat
+++ LoginServer/dist/StartLS.bat
@@ -8,7 +8,7 @@ CALL PanelLS.bat
 )
 
 ECHO Starting AION Login Server in %MODE% mode.
-JAVA %JAVA_OPTS% -cp ./lib/*;loginserver.jar com.aionemu.loginserver.LoginServer
+JAVA %JAVA_OPTS% -cp ./libs/*;Encom-Login-1.8.jar com.aionemu.loginserver.LoginServer
 SET CLASSPATH=%OLDCLASSPATH%
 IF ERRORLEVEL 1 GOTO ERROR
 IF ERRORLEVEL 0 GOTO END
